c40d55314c77b062ac953872a9a9d1c256f9d404
jatinr333 <jatin.rewri321@gmail.com>
2021-04-12 12:42:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Particles from 'react-particles-js';
+import About from './components/About';
+import Services from './components/Services'
 function App() {
   return (<>
   <Particles
@@ -22,6 +24,8 @@ function App() {
 
    <Navbar/>
    <Header/>
+   <About/>
+   <Services/>
    </>
   );
 }
